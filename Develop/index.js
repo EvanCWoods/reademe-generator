ab8eeea9ad71dcf092394f6107f22b89503daa94
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-02-03 07:50:25 +1100

    Added logic to handle multiple questions and destructuring responses

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const { title, description, installInstructions, usageInstrustions, constributionGuide } = data;
+    fs.appendFile(fileName, 
+        `# ${title} \n 
+## Description \n ${description} \n
+### Instructions \n ${installInstructions}\n
+### Usage Instructions \n ${usageInstrustions} \n
+### Contribution Guide \n ${constributionGuide}`, 
+        (res, err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize app 
+function init() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "README Title: ",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "README Description: ",
+        },
+        {
+            type: "input",
+            name: "installInstructions",
+            message: "README Installation Instructions: ",
+        },
+        {
+            type: "input",
+            name: "usageInstructions",
+            message: "README Usage Instructions: ",
+        },
+        {
+            type: "input",
+            name: "constributionGuide",
+            message: "README Usage Instructions: ",
+        },
+    ]).then( (data) => {
+        const title = data.title;
+        const description = data.description;
+        const installInstructions = data.installInstructions;
+        const usageInstructions = data.usageInstructions;
+        const constributionGuide = data.constributionGuide;
+        writeToFile("README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
